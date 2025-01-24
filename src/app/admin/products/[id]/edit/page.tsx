4b7d4db0f7c83a1381fa7907c2b3a78fac2ec275
Nana Mohamadi <adilnana78@gmail.com>
2025-01-24 13:37:20 +0100

    Admin Product Pages

---
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,20 @@
+import { PageHeader } from "@/app/admin/_components/PageHeader";
+import { ProductForm } from "../../_components/ProductForm";
+import { db } from "@/lib/db";
+
+export default async function EditProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  const product = await db.product.findUnique({
+    where: { id },
+  });
+  return (
+    <>
+      <PageHeader>Edit Product</PageHeader>
+      <ProductForm product={product} />
+    </>
+  );
+}
